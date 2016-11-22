2347e79c79fcdb581bcb38c220ad461d6b673be7
JumpingYang001 <v-zhenpy@microsoft.com>
2016-11-21 23:57:25 -0800

    Add Markers画图中文版

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# markers-cn
+# Markers画图中文版
 Forked from https://github.com/dsandler/markers, but a Chinese version:Markers画图中文版
+
+#Release apk
+https://github.com/JumpingYang001/markers-cn/blob/master/app/app-release.apk
+
+##Screen shots
+![Alt text](screenshots/a.PNG?raw=true "Title a")
+![Alt text](screenshots/b.PNG?raw=true "Title b")
---
+++ app/build/generated/source/buildConfig/release/org/dsandler/apps/markers/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.dsandler.apps.markers;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.dsandler.apps.markers";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 56;
+  public static final String VERSION_NAME = "1.2.3";
+}
